b6123095ff360bcf1865cb831c0dd433bec73c77
akateason <xietianchen@subaojaojiang.com>
2016-08-01 11:11:07 +0800

    content add content update content readnum

--- src/cn/myapp/util/XtDate.java
+++ src/cn/myapp/util/XtDate.java
@@ -2,8 +2,14 @@ package cn.myapp.util;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat; 
 
 public class XtDate {
+
+	public static String mDateStringFormat = "yyyy-MM-dd" ;
+    public static String mTimeStringFormat = "HH:mm:ss" ;
+    public static String mDateTimeStringFormat = "yyyy-MM-dd HH:mm:ss" ;
 	
 	public static Date getCurrentDayOriginalDate() {
 		Calendar calendar = Calendar.getInstance() ;
@@ -13,5 +19,20 @@ public class XtDate {
 		Date today = calendar.getTime();
 		return today ;
 	}
-		
+	
+	public static Date getNowDate() {
+		return new java.util.Date() ;  
+	}
+    
+	public static Date getDateWithString(String dateStr) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat(mDateTimeStringFormat) ;
+		java.util.Date date = sdf.parse(dateStr);   
+		return date ;
+	}
+	
+	public static String getStringWithDate(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat(mDateTimeStringFormat) ;  
+		return sdf.format(date) ;  
+	}
+	
 }
